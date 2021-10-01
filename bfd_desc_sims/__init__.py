9957b3551e4c88448270493b9c2e7a9b7a6912b6
Armstrong, Robert Ellis <armstrong46@llnl.gov>
2021-10-01 15:48:48 -0700

    New code to do neighbor subtraction

--- bfd_desc_sims/__init__.py
+++ bfd_desc_sims/__init__.py
@@ -3,4 +3,6 @@ from .grid import *
 from .grid_z import *
 from .blend import *
 from .blend_scarlet import *
+from .blend_scarlet_nn import *
 from .grid_scarlet import *
+from .grid_scarlet_nn import *
